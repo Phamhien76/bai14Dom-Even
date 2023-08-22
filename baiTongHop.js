4d4a63185a34d947c71324b7b67b06141e92d63d
Hien <phamhien4388@gmail.com>
2023-08-22 16:00:03 +0900

    bài tập tông hợp

--- baiTongHop.js
+++ baiTongHop.js
@@ -1,8 +1,8 @@
 let index = 0;
 //biến toàn cục để biết khi nào là thêm học viên mới học viên, cập nhật học viên
-let action="Create"
-  //tạo một bang ban dau
-  let arrhocVien = [];
+let action = "Create"
+//tạo một bang ban dau
+let arrhocVien = [];
 function save(event) {
     //Lấy giá trị
     event.preventDefault();
@@ -11,7 +11,7 @@ function save(event) {
     let email = document.getElementById("email");
     let phone = document.getElementById("phone");
     let address = document.getElementById("address");
-    let namVal = document.getElementById("nam").                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;
+    let namVal = document.getElementById("nam").checked;;
     let errorName = document.getElementById("errorName")
     let errorEmail = document.getElementById("errorEmail");
     let errorPhone = document.getElementById("errorPhone");
@@ -41,7 +41,7 @@ function save(event) {
         return false;
     };
 
-  
+
     //tao mang moi bằng opject
     let newArrhocVien = {
         id: id.value,
@@ -60,7 +60,7 @@ function save(event) {
         //gán cho mảng ban đầu
         arrhocVien.push(newArrhocVien);
     update(newArrhocVien, index);
-    
+
 }
 
 //gán vào danh sách
@@ -77,33 +77,43 @@ function update(newArrhocVien, index) {
   <td>${newArrhocVien.address}</td>
   <td>${newArrhocVien.gender}</td>
   <td>
-  <a href="javascript: edit(id)">edit</a>
-      <a href="" onclick="delete()">delete</a>
+  <a href="javascript: edit(newArrhocVien)">edit</a>
+  <a href="javascript: deleteDanhSach(newArrhocVien)">delete</a>
   </td>
   <td></td>
 `
     //thêm dữ liệu vào danh sách
     dongMoi.appendChild(tr);
-    edit(newArrhocVien,id)
-    
+    edit(newArrhocVien)
+
 }
+// xóa khi nhấn delete
+function deleteDanhSach (newArrhocVien){
+    newArrhocVien.id="";
+    newArrhocVien.name="";
+    newArrhocVien.email="";
+    newArrhocVien.phone="";
+    newArrhocVien.address="";
+   newArrhocVien.gender="";
+}
+
 //target: lấy được thông tin sinh viên cần cập nhật và hiển thị trên form
-function edit(newArrhocVien,id){
+function edit(newArrhocVien) {
     //lấy thông tin sinh viên theo mã sinh viên
     // let indexUpdate=getIndexStudentById(id);
     //hiển thì tông tin người dùng lên form để chỉnh sửa
-// if(indexUpdate>=0){
-    document.getElementById("id").value=newArrhocVien.id;
-    document.getElementById("name").value=newArrhocVien.name;
-    document.getElementById("email").value=newArrhocVien.email;
-    document.getElementById("phone").value=newArrhocVien.phone;
-    document.getElementById("address").value=newArrhocVien.address;
-    document.getElementById("xex").checked=newArrhocVien.gender;
-// }
-//không cho phép sửa mã sinh viên trên form khi cập nhật-readonly
-// document.getElementById("id").readOnly=true;
-action="Update";
-getIndexStudentById(newArrhocVien,id)
+    // if(indexUpdate>=0){
+    document.getElementById("id").value = newArrhocVien.id;
+    document.getElementById("name").value = newArrhocVien.name;
+    document.getElementById("email").value = newArrhocVien.email;
+    document.getElementById("phone").value = newArrhocVien.phone;
+    document.getElementById("address").value = newArrhocVien.address;
+    document.getElementById("xex").checked = newArrhocVien.gender;
+    // }
+    //không cho phép sửa mã sinh viên trên form khi cập nhật-readonly
+    // document.getElementById("id").readOnly=true;
+    action = "Update";
+    // getIndexStudentById(newArrhocVien,id)
 }
 
 
